c1f382ee86ceb40238b9b4683216ce8e83266f01
xiewulong <xiewulong@vip.qq.com>
2015-03-01 16:15:36 +0800

    fileupload

--- FileuploadAction.php
+++ FileuploadAction.php
@@ -19,12 +19,6 @@ class FileuploadAction extends Action{
 		parent::init();
 
 		$this->manager = \Yii::createObject(Yii::$app->components['fileupload']);
-		$size = [230, 500];
-		$file = [
-			'tmp' => '/home/xiewulong/com.diankego/webappManage/web/assets/upload/2015/02/28/dkg_dev_u_1425060976_cc73fbdf305b49eab7c96316ede93be2.jpg',
-			'src' => '/assets/upload/2015/02/28/dkg_dev_u_1425060976_cc73fbdf305b49eab7c96316ede93be2.jpg',
-		];
-		$_file = $this->manager->addSuf($file, $size);
 	}
 
 	public function run(){
@@ -34,6 +28,7 @@ class FileuploadAction extends Action{
 		$max = $request->post('max');
 		$type = $request->post('type');
 		$sizes = $request->post('sizes');
+		$oss = $request->post('oss');
 		$response = ['status' => 0, 'message' => \Yii::t('common', 'File upload failed') . ', ' . \Yii::t('common', 'Please try again')];
 
 		if(!empty($name) && !empty($_FILES)){
@@ -45,19 +40,27 @@ class FileuploadAction extends Action{
 			}else if(!empty($type) && !in_array($_file['type'], $this->types[$type])){
 				$response['message'] = \Yii::t('common', 'Please upload the right file type');
 			}else{
-				$file = $this->manager->createFile(array_pop(explode('.', $_file['name'])));
+				$manager = $this->manager;
+				$canOss = isset($manager->oss[$oss]);
+				$file = $manager->createFile(array_pop(explode('.', $_file['name'])));
 				if(move_uploaded_file($_file['tmp_name'], $file['tmp'])){
 					$response['status'] = 1;
 					$response['message'] = \Yii::t('common', 'File upload successful');
-					$response['data'] = ['original' => $file['src']];
+					$response['data'] = ['original' => $manager->finalFile($file, $oss)];
 					if(!empty($sizes)){
 						foreach(explode('|', $sizes) as $size){
 							$_size = explode('x', $size);
 							if(count($_size) != 2)continue;
-							$thumbnail = $this->manager->addSuf($file, $_size);
+							$thumbnail = $manager->addSuf($file, $_size);
 							Image::thumbnail($file['tmp'], $_size[0], $_size[1], 'inset')->save($thumbnail['tmp']);
-							$response['data']['t' . $size] = $thumbnail['src'];
+							$response['data']['t' . $size] = $manager->finalFile($thumbnail, $oss);
+							if($canOss){
+								unlink($thumbnail['tmp']);
+							}
 						}
+					}
+					if($canOss){
+						unlink($file['tmp']);
 					}
 				}
 			}
